7266af394c2ca324a8fe9fe72f57f29fba854a2d
Sydney Boyd <>
2021-04-19 02:28:27 +0000

    complete

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var customers = require('./customers.json');
 Write a function that canfind the average balance 
 of all of the customers in the dataset. You will 
 need to know how to find the average of a set of 
-numbers and know how to access the balance property 
+numbers and know how to access pthe balance property 
 in the object.
 
 */
@@ -17,8 +17,9 @@ in the object.
 function averageBalance(array){
   //CODE BELOW HERE
 
+for ( var i = 0; i <= average.Balance - 1; i++){
 
-
-
+console.log(Balance[]);
   //CODE ABOVE HERE
 }
+averageBalance(3690.70,)
